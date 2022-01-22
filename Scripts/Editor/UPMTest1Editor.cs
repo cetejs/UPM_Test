cad94cf73d4a7c0d90cda76fb50f5aaad046e17e
尹先军 <yinxianjun@xmfunny.com>
2022-01-22 10:19:24 +0800

    update

---
+++ Scripts/Editor/UPMTest1Editor.cs
@@ -0,0 +1,23 @@
+using UnityEditor;
+using UnityEngine;
+
+internal static class UPMTestEditor
+{
+    [MenuItem("UMP1/Test")]
+    internal static void Test() 
+    {
+        Debug.Log("UPM Test Editor");
+    }
+    
+    [MenuItem("UMP1/Test2")]
+    internal static void Test2()
+    {
+        Debug.Log("UPM Test Editor");
+    }
+    
+    [MenuItem("UMP1/Test3")]
+    internal static void Test3()
+    {
+        Debug.Log("UPM Test Editor");
+    }
+}
